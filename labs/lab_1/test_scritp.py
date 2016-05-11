35eb2a3b504d4077da08cd8cfd22238d4beaeb6a
Aleksander Andrzjewski <aa999111@gmail.com>
2016-05-11 17:29:04 +0200

    5.01

--- labs/lab_1/test_scritp.py
+++ labs/lab_1/test_scritp.py
@@ -7,11 +7,11 @@ def test(fun, *args):
     if isinstance(args[0], str):
         decoded = "".join([chr(i) for i in args[-1]])
         if res == decoded:
-            print "Yes, "+decoded.replace("my","your")
+            print "Yes, "+decoded.replace("my", "your")
         else:
-            print "No, "+decoded.replace("my","your").replace("has","has not")+" yet"
+            print "No, "+decoded.replace("my" ,"your").replace("has","has not")+" yet"
     else:
-        print "Is correct? "+ str(res == args[-1])
+        print "Is correct? " + str(res == args[-1])
     print "".join(['-' for i in range(40)])
 
 
@@ -24,22 +24,17 @@ def zadanie1(listObject):
         poprzedni = obiekt
     return wynik
 
-print zadanie1( [1, 2, 3, 3, 5, 68, 68, 24])
+print zadanie1([1, 2, 3, 3, 5, 68, 68, 24])
 test(zadanie1, [1, 2, 3, 3, 5, 68, 68, 24], [1, 2, 3, 5, 68, 24])
-
-
-
 
 def zadanie2(list1, list2):
     # type your code
     pass
-
 #test(zadanie2, [1, 2, 19, 'dd', ':P', ":("], [12,'c','5'], [1, 12, 2, 'c', 19, '5', 1, 2, 19, 'dd', ':P', ':('])
 #???
 
-
-def zadanie3(listTuples):
-    lista3 = [list(element) for element in listTuples ]
+def zadanie3(listtuples):
+    lista3 = [list(element) for element in listtuples]
     for element3 in lista3:
         element3.reverse()
     lista3.sort()
@@ -52,13 +47,25 @@ test(zadanie3, [(1, 3), (3, 3, 2), (2, 1)], [(2, 1), (3, 3, 2), (1, 3)])
 
 def zadanie4(text):
 #   lista = [kod(s) for s in lista if warunek(s)]
-    lista = text.split("ok")
-    wynikowalista=[]
+    lista = text.split( "ok" )
+    wynikowalista = []
     for element in lista:
-        podstawienie=""
+        podstawienie = ""
         for indeks in range(0,element.find("$")):
-            podstawienie+=element[indeks]
+            podstawienie += element[indeks]
         wynikowalista.append(podstawienie)
     return " ".join(wynikowalista).strip()
 
 test(zadanie4, "okmy$aiaetiaigaafbaf??a$okwatch$oafbusd$okhas$asbrsi31480$okended$aq340af", [109, 121, 32, 119, 97, 116, 99, 104, 32, 104, 97, 115, 32, 101, 110, 100, 101, 100])
+
+
+
+
+from random import randint
+
+a=randint(1,9)
+print a
+a = input("Podaj liczbe:\n")
+print "Podales "+a
+
+
